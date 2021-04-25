ccbf101b3b991cd099c56e552c21bee69395ccb9
Matheusyyes <matheus.300miranda@live.com>
2021-04-25 15:28:24 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/services/UserService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.hroauth.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.devsuperior.hroauth.entities.User;
+import com.devsuperior.hroauth.feignclients.UserFeignClient;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findbyEmail(String email) {
+		
+		User user = userFeignClient.findByEmail(email).getBody();
+		
+		if(user == null) {
+			throw new IllegalArgumentException("Email not found");
+		}
+		
+		return user;
+	}
+	
+}
