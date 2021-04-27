7954484ab9b8edaee9260452d9d250a4ebcfcf7b
Matheusyyes <matheus.300miranda@live.com>
2021-04-27 18:38:37 -0300

    db config server

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -24,9 +24,7 @@ public class WorkerResource {
 
 	private static Logger logger = org.slf4j.LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
-	private String testConfig;
-	
+
 	@Autowired
 	private Environment env;
 	
@@ -36,7 +34,7 @@ public class WorkerResource {
 	@GetMapping(value="/configs")
 	public ResponseEntity<Void> getConfigs(){
 		
-		logger.info("CONFIG = "+testConfig);
+	//logger.info("CONFIG = "+testConfig);
 		
 		return ResponseEntity.noContent().build();
 	}
